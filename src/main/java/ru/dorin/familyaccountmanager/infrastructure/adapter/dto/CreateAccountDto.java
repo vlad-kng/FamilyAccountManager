668e27291103e4bb1327b4bc8c82f5dbad897413
VTanchuk <vtanchuk@zunami.ru>
2025-05-02 14:11:37 +0200

    add swagger support

---
+++ src/main/java/ru/dorin/familyaccountmanager/infrastructure/adapter/dto/CreateAccountDto.java
@@ -0,0 +1,14 @@
+package ru.dorin.familyaccountmanager.infrastructure.adapter.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import ru.dorin.familyaccountmanager.domain.account.AccountType;
+
+public record CreateAccountDto(
+        @Schema(description = "Название счета")
+        String accountName,
+        @Schema(description = "Тип счета")
+        AccountType accountType,
+        @Schema(description = "Баланс")
+        String balance
+) {
+}
